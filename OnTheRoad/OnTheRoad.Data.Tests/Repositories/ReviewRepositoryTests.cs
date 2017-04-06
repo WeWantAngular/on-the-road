17e731cb3fc2bddd4eb172f584c828fbd2594253
GalinStoychev <stoychev.galin@gmail.com>
2017-04-06 14:29:11 +0300

    nonimportant

--- OnTheRoad/OnTheRoad.Data.Tests/Repositories/ReviewRepositoryTests.cs
+++ OnTheRoad/OnTheRoad.Data.Tests/Repositories/ReviewRepositoryTests.cs
@@ -4,7 +4,6 @@ using System.Data.Entity;
 using System.Linq;
 using NUnit.Framework;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Moq;
 using OnTheRoad.Data.Contracts;
 using OnTheRoad.Data.Models;
